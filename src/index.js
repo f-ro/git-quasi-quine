1b3e9eb33785e174219371f130149ac70411bbd6
f-ro <f.roberto.isc@gmail.com>
2021-02-21 16:17:58 -0600

    API call initial setup and test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios'
 
+let api_url = `https://api.github.com/repos`
+let user = `f-ro`
+let repo = `git-quasi-quine`
+
+function getAllCommitsForMainBranch() {
+    axios({
+        method: "GET",
+        url: `${api_url}/${user}/${repo}/commits?sha=main`,
+    }).then(rslt => {console.log(rslt.data)})
+}
+
+getAllCommitsForMainBranch() 
